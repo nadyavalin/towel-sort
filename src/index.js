dfae94b8cbe7bf2f5023e002230bbd778a6fb1e9
nadyavalin <nadejda.sergeevna.tkachuk@gmial.com>
2023-08-11 23:31:39 +0300

    feat: solve task towel-sort with discriptions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+"use strict"
+module.exports = function towelSort(matrix) {
+  // проверяем, является ли matrix undefined, null, false, пустой строкой или имеет длину 0
+  // если да, то возвращаем пустой массив, потому что нет элементов для сортировки
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+  // если matrix не является пустым массивом, используем метод reduce, чтобы пройти по каждой строке матрицы
+  return matrix.reduce((result, row, index) => {
+    if (index % 2 === 0) { // проверяем четность index
+       // если index четный, объединяем элементы row с result с помощью метода concat
+      return result.concat(row);
+    } else {
+      // если index нечетный, переворачиваем элементы row с помощью метода reverse, объединяем их с result
+      return result.concat(row.reverse()); 
+    }
+  }, []);
 }
+
+  
+  
